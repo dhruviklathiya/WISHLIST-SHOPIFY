28052eaad2d2b913bc39c34d6253bbb920ae0239
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2024-08-21 11:52:52 +0530

    Action in shopify

--- app/routes/app.task.jsx
+++ app/routes/app.task.jsx
@@ -1,13 +1,26 @@
-import { json, useLoaderData } from '@remix-run/react';
+import { json, useLoaderData, Form } from '@remix-run/react';
 import { Button, TextField } from '@shopify/polaris';
 import { useState, useCallback } from 'react';
 
 export async function loader() {
-    // Here we must return json that in future will provide data from databse
+    // Here we must return json that in future will provided by database
     // Because of database we must make this function async
+
+    // json is imported from @remix-run/react
     return json({
         name: "First-task",
         description: "Given task"
+    })
+}
+
+export async function action({ request }) {
+    // Here we must return json that in future will provided by database
+    let data = await request.formData()
+    data = Object.fromEntries(data)
+    return json({
+        status: 200,
+        mesaage: "Created successfully",
+        data
     })
 }
 
@@ -16,19 +29,26 @@ export default function task() {
     const [data, setData] = useState(loaded_data);
     return (
         <>
-            <TextField
-                label="Store name"
-                value={data.name}
-                onChange={(value) => setData({ ...data, name: value })}
-                autoComplete="off"
-            />
-            <TextField
-                label="Store Description"
-                value={data.description}
-                onChange={(value) => setData({ ...data, description: value })}
-                autoComplete="off"
-            />
-            <Button submit={true}>Click for submit</Button>
+            {/* We will use form tag but will not import it from polaris */}
+            {/* We will use form tag from @remix-run/react */}
+            {/* Reason: Complexity */}
+            <Form method='POST'>
+                <TextField
+                    label="Store name"
+                    name="Store name"
+                    value={data.name}
+                    onChange={(value) => setData({ ...data, name: value })}
+                    autoComplete="off"
+                />
+                <TextField
+                    label="Store Description"
+                    name="Store Description"
+                    value={data.description}
+                    onChange={(value) => setData({ ...data, description: value })}
+                    autoComplete="off"
+                />
+                <Button submit={true}>Click for submit</Button>
+            </Form>
         </>
     )
 }
